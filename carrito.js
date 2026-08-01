f9564df9e8a0b140273ac9b398a555958929af70
sircolitas <sircolitas@gmail.com>
2026-08-01 09:25:17 -0500

    Update translation assignment to use innerHTML
    Changed innerText to innerHTML for translation elements.

--- carrito.js
+++ carrito.js
@@ -8,7 +8,7 @@ function changeLanguage(lang) {
 
     document.querySelectorAll('[data-i18n]').forEach(el => {
         const key = el.getAttribute('data-i18n');
-        if (translations[lang] && translations[lang][key]) el.innerText = translations[lang][key];
+        if (translations[lang] && translations[lang][key]) el.innerHTML = translations[lang][key];
     });
 
     const selects = document.querySelectorAll('.size-select');
